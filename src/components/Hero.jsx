4bea430c9730bb7d9da1b5dd08edf4ebd5bce536
Michal Borysewicz <michael.borissavage@gmail.com>
2023-03-04 14:00:31 +0100

    hero component optimization for mobile devices

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,11 +4,11 @@ import Typed from "react-typed";
 const Hero = () => {
     return (
         <div className="text-slate-200">
-            <div className="mx-auto mt-[-96px] flex h-screen w-full max-w-[800px] flex-col justify-center px-4 text-center">
+            <div className="mx-auto mt-[-96px] flex h-screen w-full max-w-[800px] flex-col justify-center px-2 text-center">
                 <p className="p-2 font-bold text-tealx">
                     GROWING WITH DATA ANALYTICS
                 </p>
-                <h1 className="text-4xl font-bold sm:text-6xl md:py-6 md:text-7xl">
+                <h1 className="text-4xl font-bold sm:text-6xl md:text-7xl">
                     Grow with data.
                 </h1>
                 <div className="flex items-center justify-center">
@@ -23,11 +23,11 @@ const Hero = () => {
                         loop
                     />
                 </div>
-                <p className="text-lg font-bold text-slate-500 md:text-xl">
-                    Monitor your data analytics to increase revenue
+                <p className="md:py-2 font-bold text-slate-500 text-lg">
+                    Monitor your&nbsp;data analytics to increase revenue
                     for&nbsp;BTB, BTC & SAAS platforms.
                 </p>
-                <button className="my-6 mx-auto w-[200px] rounded-lg bg-tealx py-3 font-bold text-black">
+                <button className="my-6 md:my-2 mx-auto w-[200px] rounded-lg bg-tealx py-3 font-bold text-black">
                     Get Started
                 </button>
             </div>
